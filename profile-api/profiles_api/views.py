cae574b086aa679395636bc0e9fd53371ce197a7
Sanjay Sikdar <znasofficial@gmail.com>
2020-06-13 00:16:25 +0530

    added search

--- profile-api/profiles_api/views.py
+++ profile-api/profiles_api/views.py
@@ -1,11 +1,12 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from profiles_api import serializers
 from rest_framework import viewsets
-
-from profiles_api import models
 from rest_framework.authentication import  TokenAuthentication
+from rest_framework import filters
+
+from profiles_api import serializers
+from profiles_api import models
 from . import permissions
 
 class HelloApiView(APIView):
@@ -96,3 +97,5 @@ class UserProfileViewSet(viewsets.ModelViewSet):
 	queryset = models.UserProfile.objects.all()
 	authentication_classes = (TokenAuthentication,)
 	permission_classes = (permissions.UpdateOwnProfile,)
+	filter_backends = (filters.SearchFilter,)
+	search_fields = ('name','email',)
