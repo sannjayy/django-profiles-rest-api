73eb42db2d1f5c7437d91a45d71b6d48e49fe748
Sanjay Sikdar <znasofficial@gmail.com>
2020-06-18 12:28:07 +0530

    deploy

--- profile-api/profile_rest/settings.py
+++ profile-api/profile_rest/settings.py
@@ -23,7 +23,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '9$msfd)=)bn5w8d%@kpe9of+$$jwuh1z31y-=mi0(%81uqsp#e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG', 1)))
 
 ALLOWED_HOSTS = []
 
@@ -124,3 +125,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = 'profiles_api.UserProfile'
+
+STATIC_ROOT = 'static/'
